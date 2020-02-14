d4c38f650e17e24bde19e969a8e722cd2196fe61
Hambe <petersenmaurice@gmail.com>
2020-02-14 00:23:06 +0000

    funcionalidade de login básico

---
+++ filas_app.sql
@@ -0,0 +1,70 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Feb 14, 2020 at 01:21 AM
+-- Server version: 10.4.8-MariaDB
+-- PHP Version: 7.3.10
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `filas_app`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `nome` varchar(255) DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `password` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `nome`, `email`, `password`) VALUES
+(1, 'test', 't@t', '$2b$10$tpos5kfjbhU3KMULkHnSV.zJk/kyP7t0nEiYd03LBN/rRC9t/D0S.'),
+(2, 'admin', 'admin@admin.com', '$2b$10$0vPhHpAH5NraHHhKU4mEseWxwaowoaQnMc9NHJPO2h8bAoIoz3cD6');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `email` (`email`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
--- server.js
+++ server.js
@@ -1,1 +1,100 @@
 //npm run devStart
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const server = require('http').createServer(app);
+const io = require('socket.io')(server);
+const session = require('express-session');
+
+const bcrypt = require('bcrypt');
+const mysql = require('mysql');
+const con = mysql.createConnection({
+	host: 'localhost',
+	user: 'root',
+	password: '',
+	database: 'filas_app'
+});
+
+app.use(express.static(path.join(__dirname, 'public')));
+// app.engine('html', require('ejs').renderFile);
+app.set('view-engine', 'ejs');
+app.use(express.urlencoded({ extended: false }));
+app.use(
+	session({
+		secret: 'jeff johnson',
+		resave: false,
+		saveUninitialized: false
+	})
+);
+
+app.get('/', (req, res) => {
+	res.render('login.ejs');
+	// res.render('index.ejs');
+});
+
+app.get('/test', (req, res) => {
+	if (req.session.loggedin) {
+		res.render('test.ejs', { nome: req.session.nome });
+	} else {
+		res.redirect('/');
+	}
+});
+
+app.get('/logout', (req, res) => {
+	// TODO session destroy
+	res.redirect('/');
+});
+
+app.get('/login', (req, res) => {
+	res.render('login.ejs');
+});
+
+app.post('/login', (req, res) => {
+	const email = req.body.email;
+	const password = req.body.password;
+	async function checkUser(email, password) {
+		con.query('SELECT nome, password FROM users WHERE email = ?', [ email ], async function(err, result) {
+			const hashedPassword = result[0].password;
+
+			const match = await bcrypt.compare(password, hashedPassword);
+
+			if (match) {
+				req.session.loggedin = true;
+				req.session.nome = result[0].nome;
+				res.redirect('/test');
+			} else {
+				// TODO Msg de erro
+				res.redirect('/login');
+			}
+		});
+	}
+	checkUser(email, password);
+});
+
+app.get('/register', (req, res) => {
+	res.render('register.ejs');
+});
+
+app.post('/register', async (req, res) => {
+	try {
+		const hashedPassword = await bcrypt.hash(req.body.password, 10);
+
+		var sql =
+			'INSERT INTO users (nome, email, password) VALUES ("' +
+			req.body.nome +
+			'", "' +
+			req.body.email +
+			'", "' +
+			hashedPassword +
+			'")';
+		con.query(sql, function(err, result) {
+			if (err) throw err;
+		});
+
+		res.redirect('/login');
+	} catch (err) {
+		res.redirect('/register');
+	}
+});
+server.listen(3000);
