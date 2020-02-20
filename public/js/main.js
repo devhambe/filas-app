51b28f4a9432e33d16cd1b1146b38709e1772518
Hambe <petersenmaurice@gmail.com>
2020-02-20 23:16:07 +0000

    inicio layout index

---
+++ public/js/main.js
@@ -0,0 +1,26 @@
+function checkTime(i) {
+	if (i < 10) {
+		i = '0' + i; // acrescenta um zero à frente de números<10
+	}
+	return i;
+}
+
+function getTime() {
+	var today = new Date();
+	var h = today.getHours();
+	var m = today.getMinutes();
+	var s = today.getSeconds();
+	m = checkTime(m);
+	s = checkTime(s);
+	document.getElementById('tempo-atual').innerHTML = h + ':' + m + ':' + s;
+	t = setTimeout(function() {
+		getTime();
+	}, 500);
+}
+
+function getDate() {
+	const options = { weekday: 'long', month: 'long', year: 'numeric', day: 'numeric' };
+	const today = new Date();
+	const str = today.toLocaleDateString('pt-PT', options);
+	document.getElementById('data-atual').innerHTML = str.toUpperCase();
+}
